22d80f8c70dff6f3546c55a3024709ac3a6d0a6f
narendrakumarft <narendrkumarofficial@gmail.com>
2025-10-29 21:56:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>Hi Narendrakumar!</h1>
+      <h1 className='text-green-500'>nk!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
